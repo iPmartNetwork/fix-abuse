f1fe5bd2203244623f0cfb1854f78edd34826774
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-07-18 06:26:17 +0330

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -77,7 +77,7 @@ EOF
     echo -e "${YELLOW}Please choose an option:${NC}"
     echo "══════════════════════════════════════════════════════════════════════════════════════"
     echo -e "${cyan}| 1  - Abuse Fixer"
-    echo -e "${White}| 2  - Status "
+    echo -e "${White}| 2  - Disable Rule "
     echo -e "${cyan}| 3  - Unistall"
     echo -e "${White}| 0  - Exit"
     echo "══════════════════════════════════════════════════════════════════════════════════════"
@@ -89,7 +89,7 @@ EOF
         install_fixer
         ;;
     2)
-        echo "simple 2"
+        disable_ip
         ;;
     0)
         echo -e "${YELLOW}Exit...${NC}"
@@ -115,4 +115,43 @@ install_fixer(){
 
 }
 
+disable_rule(){
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 172.16.0.0/12
+ufw deny out from any to 192.168.0.0/16
+ufw deny out from any to 100.64.0.0/10
+ufw deny out from any to 198.18.0.0/15
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 102.236.0.0/16
+ufw deny out from any to 2.60.0.0/16
+ufw deny out from any to 5.1.41.0/12
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 172.0.0.0/8
+ufw deny out from any to 192.0.0.0/8
+ufw deny out from any to 102.0.0.0/8
+ufw deny out from any to 200.0.0.0/8
+ufw deny out from any to 102.0.0.0/8
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 100.64.0.0/10
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 198.18.0.0/15
+ufw deny out from any to 198.51.100.0/24
+ufw deny out from any to 203.0.113.0/24
+ufw deny out from any to 224.0.0.0/4
+ufw deny out from any to 240.0.0.0/4
+ufw deny out from any to 255.255.255.255/32
+ufw deny out from any to 192.0.0.0/24
+ufw deny out from any to 192.0.2.0/24
+ufw deny out from any to 127.0.0.0/8
+ufw deny out from any to 127.0.53.53
+ufw deny out from any to 192.168.0.0/16
+ufw deny out from any to 0.0.0.0/8
+ufw deny out from any to 172.16.0.0/12
+ufw deny out from any to 224.0.0.0/3
+ufw deny out from any to 192.88.99.0/24
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 198.18.140.0/24
+ufw deny out from any to 102.230.9.0/24
+ufw deny out from any to 102.233.71.0/24
+}
 loader
